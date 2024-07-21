85320059918fd8db2c96ef27c532bdc3431a9407
KyryloDolia <fr1ghtn1ght2101@gmail.com>
2024-07-21 21:21:39 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,5 +1,27 @@
 from pathlib import Path
 
+def get_cats_info(path: str) -> list:
+    
+    path = Path(path)
+    if not path.exists():
+        print("Файл за вказаним шляхом не знайдений")
+        return None
+
+    cat_dicts = []
+    with open(path, 'r', encoding='utf-8') as fh:
+
+        lines = [el.strip().split(',') for el in fh.readlines()]
+        for line in lines:
+            try:
+                cat_dicts.append({'id': line[0], 'name': line[1], 'age': int(line[2])})
+            except (IndexError, ValueError):
+                print(f"Невірні дані в рядку: {lines.index(line)+1}")  
+                
+    return cat_dicts
+
+
+
+
 def total_salary(path: str) -> tuple:
     
     path = Path(path)
@@ -9,6 +31,21 @@ def total_salary(path: str) -> tuple:
 
     with open(path, 'r', encoding='utf-8') as fh:
         lines = [el.strip().split(',') for el in fh.readlines()]
-        summ = sum([int(line[1]) for line in lines])
-        avg = summ // len(lines)
+        salaries = []
+        for line in lines:
+            try:
+                salary = int(line[1])
+                if salary <= 0:
+                    print(f"Поминалка в рядку: {lines.index(line)+1}. Число не може бути негативним")
+                    return None
+                salaries.append(salary)
+            except (IndexError, ValueError):
+                print(f"Invalid data in line {lines.index(line)+1}")
+                return None
+
+        if not salaries:
+            return None
+
+        summ = sum(salaries)
+        avg = summ // len(salaries)
         return summ, avg
